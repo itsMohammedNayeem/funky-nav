b68c85cf09e298f25b0ef4d175b9f0f8542170af
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2024-01-14 13:51:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <>
+      <nav>
+        <ul className="nav-list">
+          <li className="active">
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="about.html">About</a>
+          </li>
+          <li>
+            <a href="blog.html">Blog</a>
+          </li>
+          <li>
+            <a href="#">Another</a>
+          </li>
+          <li>
+            <a href="#">Page</a>
+          </li>
+          <li>
+            <a href="#">And another page</a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
